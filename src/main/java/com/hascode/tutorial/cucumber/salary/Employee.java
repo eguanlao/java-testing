2605d6d40205c1dfb43b85850930231cc56d7dfd
Erick Guanlao <ericguanlao@gmail.com>
2015-08-27 20:58:16 +1000

    Initial commit.

---
+++ src/main/java/com/hascode/tutorial/cucumber/salary/Employee.java
@@ -0,0 +1,21 @@
+package com.hascode.tutorial.cucumber.salary;
+
+public class Employee {
+
+  private int id;
+  private String user;
+  private float salary;
+
+  public int getId() {
+    return id;
+  }
+
+  public float getSalary() {
+    return salary;
+  }
+
+  public void setSalary(float salary) {
+    this.salary = salary;
+  }
+
+}
